cb1006c40d438064abb7db1768b3e72d3a17aaf3
Ludvík Prokopec <prokolud@fit.cvut.cz>
2022-12-02 17:32:05 +0100

    Fix index page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,16 +1,7 @@
 <script lang="ts">
 	import Layout from '../lib/components/Layout.svelte'
-	import { createCollectionSubscriber } from '$lib/database'
-
-	const names = createCollectionSubscriber('6388a5e235c0c0fc8054', '6388a5e930cfd8a7ae56')
 </script>
 
 <Layout>
 	<h1>Home</h1>
-
-	<div>
-		{#each $names as { name }}
-			<p>{name}</p>
-		{/each}
-	</div>
 </Layout>
