0ed79810da26f2f30f69f38c66a04cbaac0df446
Ludvík Prokopec <prokolud@fit.cvut.cz>
2022-12-23 13:21:31 +0100

    add apache htaccess

--- src/lib/components/Common.ts
+++ src/lib/components/Common.ts
@@ -3,13 +3,12 @@ import Input from './Common/Input.svelte';
 import InputWrappper from './Common/InputWrappper.svelte';
 import Loading from './Common/Loading.svelte';
 import SOffline from './Common/SOffline.svelte';
-import Link from './Common/Link.svelte';
 import Sortable from './Common/Sortable.svelte';
 import Radio from './Common/Radio.svelte';
 import Checkbox from './Common/Checkbox.svelte';
 import Switch from './Common/Switch.svelte';
 import Select from './Common/Select.svelte';
 
-import { Body, Html, PwaInstaller } from './Common/Meta';
+export { Body, Html, PwaInstaller } from './Common/Meta';
 
-export { Button, Input, InputWrappper, Loading, SOffline, Body, Html, PwaInstaller, Link, Sortable, Switch, Radio, Checkbox, Select };
+export { Button, Input, InputWrappper, Loading, SOffline, Sortable, Switch, Radio, Checkbox, Select };
--- src/lib/components/Common/Button.svelte
+++ src/lib/components/Common/Button.svelte
@@ -1,13 +1,32 @@
 <script lang="ts">
-	export let href: string = null
+	import { link } from '$lib/router'
+
+	export let href: string | null = null
+
 	let className = ''
 	export { className as class }
+
+	const isValidHttpUrl = (string) => {
+		let url: URL
+		try {
+			url = new URL(string)
+		} catch (_) {
+			return false
+		}
+		return url.protocol === 'http:' || url.protocol === 'https:'
+	}
 </script>
 
-{#if href !== null}
-	<a {href} class={className}>
-		<slot />
-	</a>
+{#if href}
+	{#if isValidHttpUrl(href)}
+		<a {href} class={className}>
+			<slot />
+		</a>
+	{:else}
+		<a {href} class={className} use:link>
+			<slot />
+		</a>
+	{/if}
 {:else}
 	<button class={className} on:click>
 		<slot />
